16fe22b580ba99c171c7388bc477a084c9661546
MaSTeR2W <94644674+MaSTeR2W@users.noreply.github.com>
2023-04-08 19:28:47 +0200

    Update myCrypto.js

--- utilities/myCrypto.js
+++ utilities/myCrypto.js
@@ -1,43 +1,52 @@
 const crypto = require("crypto");
-const {promisify} = require("util");
+const { promisify } = require("util");
 const key = process.env.ENCRYPT_SECRET;
-const iv = Buffer.from(process.env.IV , "utf8");
+const iv = Buffer.from(process.env.IV, "utf8");
 const chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-
-exports.hashPassword = async(password , salt)=>{
-    const pass = await promisify(crypto.scrypt)(password , salt , 256);
-    return (await promisify(crypto.scrypt)(pass , process.env.PAPPER , 192)).toString("base64url");
-}
-
-exports.randomChar = ()=>{
-    return chars[Math.floor(Math.random() * 62)];
+exports.hashPassword = async (password, salt) => {
+  const pass = await promisify(crypto.scrypt)(password, salt, 256);
+  return (
+    await promisify(crypto.scrypt)(pass, process.env.PAPPER, 192)
+  ).toString("base64url");
 };
 
-exports.randomString = (length , replace)=>{
-    length = length / 8 * 6;
-    const random = crypto.randomBytes(length).toString("base64url");
-    if(replace){
-        let newText = '';
-        for(let i = 0 ; i < random.length ; i++){
-             newText += random[i] == "-" || random[i] == "_"? this.randomChar():random[i];
-        };
-        return newText;
-    };
-    return random;
+exports.randomChar = () => {
+  return chars[Math.floor(Math.random() * 62)];
 };
 
-
-exports.aesEncrypt = async(text)=>{
-    const aes_enc = crypto.createCipheriv('aes256' , key , iv);
-    let encrypted = aes_enc.update(text, 'utf8', 'base64');
-    encrypted += aes_enc.final('base64');
-    return encrypted;
+exports.randomString = (length, replace) => {
+  length = (length / 8) * 6;
+  const random = crypto.randomBytes(length).toString("base64url");
+  if (replace) {
+    let newText = "";
+    for (let i = 0; i < random.length; i++) {
+      newText +=
+        random[i] == "-" || random[i] == "_" ? this.randomChar() : random[i];
+    }
+    return newText;
+  }
+  return random;
 };
 
-exports.aesDecrypt = async(cipherText)=>{
-    const aes_dec = crypto.createDecipheriv('aes256' , key , iv);
-    let decrypted = aes_dec.update(cipherText, 'base64', 'utf8');
-    return (decrypted + aes_dec.final('utf8'));
+exports.aesEncrypt = async (text) => {
+  const aes_enc = crypto.createCipheriv("aes256", key, iv);
+  let encrypted = aes_enc.update(text, "utf8", "base64");
+  encrypted += aes_enc.final("base64");
+  return encrypted;
 };
 
+exports.aesDecrypt = async (cipherText) => {
+  const aes_dec = crypto.createDecipheriv("aes256", key, iv);
+  let decrypted = aes_dec.update(cipherText, "base64", "utf8");
+  return decrypted + aes_dec.final("utf8");
+};
+
+exports.randomNumbers = (length) => {
+  let random = "";
+  for (let i = 0; i < length; i++) {
+    random += numbers[Math.floor(Math.random() * 10)];
+  }
+  return random;
+};
